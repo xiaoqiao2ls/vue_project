335bf10c9959f48867286c9a34d7528f29735182
小桥 <xiaoqiao@qq.com>
2017-08-11 16:07:10 +0800

    初始化仓库，首次提交手脚架

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+/**
+ * Created by Administrator on 2017/8/9.
+ */
+"use strict";
+//引入node内置模块path
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+//普通  exports.min = function(){}
+
+//es6   export function xxx(){}
+//      export default function(){}
+
+module.exports = {
+    //打包入口文件
+    entry:"./src/js/main.js",
+    //输出文件路径
+    output:{
+        path:path.resolve(__dirname,"./dist"),
+        filename:"build.js"
+    },
+    //配置添加各种插件
+    plugins:[
+        new htmlWebpackPlugin({
+            template:"./src/index.html",  //原html
+            filename:"index.html",//新文件名
+            injext:"body"  //脚本注入到页面中
+        })
+    ],
+    module:{
+            rules:[
+                {
+                    test:/\.css$/,
+                    use:[
+                        'style-loader',
+                        'css-loader',
+                    ]
+                },
+                {
+                    test:/\.less$/,
+                    use:[
+                        'style-loader',
+                        'css-loader',
+                        'less-loader'
+                    ]
+                },
+                {
+                    test:/\.scss$/,
+                    use:[
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test:/\.tpl$/,
+                    use:[
+                        "html-loader"
+                    ]
+                },
+                {
+                    test:/\.(png|jpg|gif)$/,
+                    use:[
+                        {loader:'url-loader',options:{limit:5000}},
+                        'image-webpack-loader'
+                    ]
+                }
+            ]
+    }
+}
+
+
+
